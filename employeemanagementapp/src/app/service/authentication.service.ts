bab21f8a9951afe1d19cdec460ab169fdbb3d98f
Alex Zaharioiu <alexzaharioiu7@gmail.com>
2021-03-27 22:30:34 +0200

    Implemented Login service call and created User class

--- employeemanagementapp/src/app/service/authentication.service.ts
+++ employeemanagementapp/src/app/service/authentication.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class AuthenticationService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>
+    (`${this.host}/user/login`, user, {observe: 'response'});
+  }
 }
