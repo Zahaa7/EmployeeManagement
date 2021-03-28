c9e8ed549460388a4588e9e8fc8ebbc230095826
Alex Zaharioiu <alexzaharioiu7@gmail.com>
2021-03-28 21:55:46 +0300

    Implemented User list/add/update/reset password & update profile image  service calls

--- employeemanagementapp/src/app/service/user.service.ts
+++ employeemanagementapp/src/app/service/user.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpResponse, HttpErrorResponse, HttpEvent } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) {}
+
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/user/list`);
+  }
+
+  public addUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/add`, formData);
+  }
+
+  public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/update`, formData);
+  }
+
+  public resetPassword(email: string): Observable<any | HttpErrorResponse> {
+    return this.http.get(`${this.host}/user/reset-password/${email}`);
+  }
+
+  public updateProfileImage(formData: FormData): Observable<HttpEvent<User> | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/updateProfileImage`, formData, 
+    {reportProgress: true,
+      observe: 'events'
+    });
+  }
 }
